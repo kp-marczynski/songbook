a1b1ae636a9d9ada1a2c317f8544b41cc083eed4
kp-marczynski <kp.marczynski@gmail.com>
2019-12-13 21:36:22 +0100

    fixed list update on back button

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
 import {StorageHelperService} from '../../../services/storage-helper.service';
+import {HostListener} from '@angular/core';
 
 @Component({
     selector: 'app-song-list',
@@ -14,17 +15,37 @@ export class SongListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.storageHelperService.getSongIndex().then(res => {
-                this.songIndex = res;
-                this.songIndex.sort((a, b) =>
-                    this.compareStrings(a.author, b.author) === 0
-                        ? this.compareStrings(a.title, b.title)
-                        : this.compareStrings(a.author, b.author));
-            }
-        );
+        this.loadSongs();
+    }
+
+    loadSongs(): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.storageHelperService.getSongIndex().then(res => {
+                    this.songIndex = res;
+                    this.sortSongList();
+                    resolve();
+                }
+            );
+        });
+    }
+
+    sortSongList() {
+        this.songIndex.sort((a, b) =>
+            this.compareStrings(a.author, b.author) === 0
+                ? this.compareStrings(a.title, b.title)
+                : this.compareStrings(a.author, b.author));
     }
 
     compareStrings(a: string, b: string): number {
         return a.localeCompare(b, 'en', {sensitivity: 'base'});
+    }
+
+    doRefresh(event) {
+        this.loadSongs().then(() => event.target.complete());
+    }
+
+    @HostListener('window:popstate', ['$event'])
+    onPopState(event) {
+        this.loadSongs();
     }
 }
