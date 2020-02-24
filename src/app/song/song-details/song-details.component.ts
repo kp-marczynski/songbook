50a0123e9de03aad2441c6fe163c776b3c157d9c
kp-marczynski <kp.marczynski@gmail.com>
2020-02-24 12:13:24 +0100

    refactor for song details

--- src/app/song/song-details/song-details.component.ts
+++ src/app/song/song-details/song-details.component.ts
@@ -8,6 +8,7 @@ import {CampfireService} from '../../../services/campfire.service';
 import {IChordProGroup} from '../../../model/chord-pro-group.model';
 import {ChordProService} from '../../../services/chord-pro.service';
 import {StorageKeys} from '../../../model/storage-keys.model';
+import {ToastHelperService} from '../../../services/toast-helper.service';
 
 @Component({
     selector: 'app-song-details',
@@ -33,9 +34,9 @@ export class SongDetailsComponent implements OnInit, AfterViewInit {
         private campfireService: CampfireService,
         private chordProService: ChordProService,
         private route: ActivatedRoute,
-        private toastController: ToastController,
         private router: Router,
-        private routerExtService: RouterExtService) {
+        private routerExtService: RouterExtService,
+        private toastHelperService: ToastHelperService) {
     }
 
     ngOnInit(): void {
@@ -47,24 +48,45 @@ export class SongDetailsComponent implements OnInit, AfterViewInit {
             if (this.router.url.includes(StorageKeys.CAMPFIRE)) {
                 this.guestMode = true;
                 document.body.classList.toggle('dark', true);
-                this.campfireService.getCurrentSongFromFirebase(uuid).subscribe(res => {
-                    console.log(res);
-                    this.song = res;
-                    this.formattedContent = this.chordProService.parseChordPro(this.song.content);
-                });
+                this.campfireService.getCurrentSongFromFirebase(uuid).subscribe(res => this.displaySong(res));
             } else {
-                this.songService.getSong(params.get('uuid')).then(res => {
-                    this.song = res;
-                    if (!this.song) {
-                        this.previousState();
-                    } else {
-                        console.log(this.song);
-                        this.formattedContent = this.chordProService.parseChordPro(this.song.content);
-                    }
-                });
+                this.songService.getSong(params.get('uuid')).then(res => this.displaySong(res));
             }
         });
+        this.registerScrollListeners();
+    }
 
+    private displaySong(song: ISong) {
+        this.song = song;
+        if (!this.song) {
+            this.previousState();
+        } else {
+            this.formattedContent = this.chordProService.parseChordPro(this.song.content);
+        }
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            const prevUrl = this.routerExtService.getPreviousUrl();
+            this.previousUrl = prevUrl ? prevUrl : '/tabs/song';
+        });
+    }
+
+    addSongToQueue() {
+        this.campfireService.addToQueue(this.song);
+        this.toastHelperService.presentToast('Song added to queue');
+    }
+
+    removeSong() {
+        this.songService.removeSong(this.song);
+        this.router.navigate(['/tabs/song']);
+    }
+
+    previousState = () => this.router.navigate([this.previousUrl]);
+
+    setCurrentSong = () => this.campfireService.setCurrentSong(this.song);
+
+    private registerScrollListeners() {
         window.addEventListener('wheel', event => {
             if (!this.wheeling) {
                 if (this.isScrolling) {
@@ -99,53 +121,27 @@ export class SongDetailsComponent implements OnInit, AfterViewInit {
         this.triggerAutoScroll();
     }
 
-    ngAfterViewInit(): void {
-        setTimeout(() => {
-            const prevUrl = this.routerExtService.getPreviousUrl();
-            this.previousUrl = prevUrl ? prevUrl : '/tabs/song';
-        });
-    }
-
-    async presentToast() {
-        this.campfireService.addToQueue(this.song);
-        const toast = await this.toastController.create({
-            message: 'Song added to queue',
-            duration: 2000
-        });
-        toast.present();
-    }
-
-    remove() {
-        this.songService.removeSong(this.song);
-        this.router.navigate(['/tabs/song']);
-    }
-
-    previousState() {
-        this.router.navigate([this.previousUrl]);
-    }
-
-    setCurrentSong() {
-        this.campfireService.setCurrentSong(this.song);
-    }
-
     triggerAutoScroll() {
         this.content.getScrollElement().then((element: HTMLElement) => {
             this.isScrolling = !this.isScrolling;
-            const scrollableHeight = document.getElementById('song-details-list').scrollHeight
-                + element.getBoundingClientRect().top + element.getBoundingClientRect().bottom
-                - 2 * element.offsetHeight;
-            this.scroll(scrollableHeight, element.scrollTop);
+            this.performScroll(this.calculateScrollableHeight(element), element.scrollTop);
         });
     }
 
-    private scroll(scrollableHeight: number, scrollOffset: number) {
+    private calculateScrollableHeight(element: HTMLElement): number {
+        return document.getElementById('song-details-list').scrollHeight
+            + element.getBoundingClientRect().top + element.getBoundingClientRect().bottom
+            - 2 * element.offsetHeight;
+    }
+
+    private performScroll(scrollableHeight: number, scrollOffset: number) {
         if (this.isScrolling) {
             scrollOffset += (this.scrollSpeed + 5) / 50;
             if (scrollOffset >= scrollableHeight) {
                 this.isScrolling = false;
             }
             this.content.scrollToPoint(0, scrollOffset).then(() => {
-                setTimeout(() => this.scroll(scrollableHeight, scrollOffset));
+                setTimeout(() => this.performScroll(scrollableHeight, scrollOffset));
             });
         }
     }
---
+++ src/services/toast-helper.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {ToastController} from '@ionic/angular';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ToastHelperService {
+
+    constructor(private toastController: ToastController) {
+    }
+
+    async presentToast(message: string, duration?: number) {
+        const toast = await this.toastController.create({
+            message,
+            duration: !!duration ? duration : 2000
+        });
+        toast.present();
+    }
+}
